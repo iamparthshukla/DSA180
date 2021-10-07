3065bbc147f54ae5188c2f81200df804e2d90553
Parth <parthshuklaa@gmail.com>
2021-10-07 14:22:01 +0530

    questions 1 and 2

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,43 @@
-#include <iostream>
+// sort color: sort array of 0's, 1's and 2's
+// most optimised solution: Dutch nation flag algo
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "iostream"
+#include "algorithm"
+using namespace std;
+
+void sort(int arr[], int n) {
+    int lo = 0;
+    int mid = 0;
+    int hi  = n-1;
+
+    while (mid<=hi) {
+        switch (arr[mid]) {
+            case 0:
+                swap(arr[lo], arr[mid]);
+                lo++;
+                mid++;
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(arr[mid], arr[hi]);
+                hi--;
+                break;
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter the size: \n";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the elements: \n";
+    for (int i=0; i<size; i++)
+        cin>>arr[i];
+    sort(arr, size);
+    for (int i=0; i<size; i++)
+        cout<<arr[i];
 }
--- 2.cpp
+++ 2.cpp
@@ -1,4 +1,37 @@
-//
-// Created by Parth on 13/09/21.
-//
+// repeating and missing numbers
+// most optimised solution: summation approach
+
+#include "iostream"
+using namespace std;
+
+void missingDuplicate (const int arr[], int n)
+{
+    int sum, sumSq;
+    int i = 0;
+    sum = n*(n+1)/2;
+    sumSq = n*(n+1)*((2*n)+1)/6;
+    while (i<n){
+        sum -= arr[i];
+        sumSq -= (arr[i]*arr[i]);
+        i++;
+    }
+
+    int missingNum = ((sumSq/sum)+sum)/2;
+    int duplicateNum = missingNum - sum;
+
+    cout<<"Missing Number : "<<missingNum<<"\n";
+    cout<<"Duplicate Number : "<<duplicateNum<<"\n";
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter the size:\n";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the elements:";
+    for (int i=0; i<size; i++)
+        cin>>arr[i];
+    missingDuplicate(arr, size);
+}
 
